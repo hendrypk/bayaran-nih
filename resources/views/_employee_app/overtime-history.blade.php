202e94529760216889d5e4485f28a3e5f5c5cea8
hendrypk <153420667+hendrypk@users.noreply.github.com>
2024-12-05 11:48:11 +0700

    Revert "validator add update employee, date filter app"

--- resources/views/_employee_app/overtime-history.blade.php
+++ resources/views/_employee_app/overtime-history.blade.php
@@ -1,7 +1,7 @@
 @extends('_employee_app._layout_employee.main')
 @section('header.title', 'Overtime History')
 @include('_employee_app._layout_employee.header')
- {{-- @section('header')
+<!-- @section('header')
 <div class="appHeader blue text-light">
         <div class="left">
             <a href="{{ route('employee.app') }}" class="headerButton goBack">
@@ -11,13 +11,10 @@
         <div class="pageTitle"> {{ Auth::user()->name }} Presences History </div>
         <div class="right"></div>
     </div>
-@endsection  --}}
+@endsection -->
 
 @section('content')
 <div class="presence">
-    <x-date-filter action="{{ route('overtime.list') }}" 
-                    :startDate="request()->get('start_date')" 
-                    :endDate="request()->get('end_date')" />
     <div class="card">
         <div class="card-body">
             <table class="table">
@@ -32,15 +29,15 @@
                 </thead>
                 <tbody>
                     
-                    @foreach($overtime as $no=>$overtime)
                     <tr>
+                    @foreach($overtime as $no=>$overtime)
                         <td>{{ $no+1 }}</td>
-                        <td>{{ \Carbon\Carbon::parse($overtime->date)->format('d-m-Y') }}</td>
+                        <td>{{ $overtime->date }}</td>
                         <td>{{ $overtime->start_at }}</td>
                         <td>{{ $overtime->end_at ?? '-' }}</td>
                         <td>{{ $overtime->total ?? '-' }}</td>
+                        @endforeach
                     </tr>
-                    @endforeach
                     
                 </tbody>
             </table>
