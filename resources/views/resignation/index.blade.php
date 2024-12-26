77cd61c8e376c3c10e26fd7153617cd0e489d8ee
“[Hendry <hendryputra934@gmail.com>
2024-12-26 15:29:21 +0700

    default reject overtime, add resign note

--- resources/views/resignation/index.blade.php
+++ resources/views/resignation/index.blade.php
@@ -56,6 +56,7 @@
                         name: '{{ $data->name }}',
                         category: '{{ $data->resignation }}',
                         date: '{{ $data->resignation_date }}',
+                        note: '{{ $data->resignation_note }}',
                     })">
                     <i class="ri-edit-box-fill"></i>
                 </button>
@@ -87,6 +88,7 @@ function openResignModal(action, data = {}) {
         $('#name').val('');
         $('#category').val('');
         $('#date').val('');
+        $('#note').val('');
         $('#name').prop('disabled', false);
     } else if (action === 'edit') {
         $('#resignForm').attr('action', "{{ route('resignation.update') }}");
@@ -94,6 +96,7 @@ function openResignModal(action, data = {}) {
         $('#name').val(data.id);
         $('#category').val(data.category);
         $('#date').val(data.date);
+        $('#note').val(data.note);
         $('#name').prop('disabled', false);
     }
 
