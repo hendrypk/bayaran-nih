8c57abe49371ea2be6524dbb30ef88a5bd8ecb39
Hendry Putra Kurniawan <hendryputra934@gmail.com>
2025-11-03 10:31:06 +0700

    fix: employee kpi, pa, bobot kpi

--- resources/views/_layout/main.blade.php
+++ resources/views/_layout/main.blade.php
@@ -73,12 +73,13 @@
   </main>
 
   <!-- ======= Footer ======= -->
-  <footer id="footer" class="footer">
+  {{-- <footer id="footer" class="footer">
     <div class="credits">
       Powered by <a href="https://hendrypk.my.id/">hendrypk</a>
     </div>
     <div class="release-tag" id="releaseList"></div>
-  </footer><!-- End Footer -->
+  </footer> --}}
+  <!-- End Footer -->
 
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
