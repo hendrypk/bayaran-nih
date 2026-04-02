d86c03e9d3da8a1dfe75b069281209086a1f5076
Hendry Putra Kurniawan <hendryputra934@gmail.com>
2026-04-02 16:52:03 +0700

    fix: download final grade

--- app/Exports/FinalGradeExport.php
+++ app/Exports/FinalGradeExport.php
@@ -34,8 +34,8 @@ class FinalGradeExport implements FromCollection, WithHeadings, WithStyles
                 $employee->name, // Name
                 $this->selectedMonth, // Month
                 $this->selectedYear, // Year
-                $employee->kpiResults->isNotEmpty() ? $employee->kpiResults->first()->final_kpi : 0, // KPI
-                $employee->paResults->isNotEmpty() ? $employee->paResults->first()->final_pa : 0, // PA
+                $employee->kpiResults->isNotEmpty() ? $employee->kpiResults->first()->grade : 0, // KPI
+                $employee->paResults->isNotEmpty() ? $employee->paResults->first()->grade : 0, // PA
                 $employee->finalGrade, // Final Grade
             ];
         });
